224c86012161274a49569d0fa03620f88cc62299
Shruti Sachan <shrutivsachan@gmail.com>
2024-02-25 22:38:31 -0800

    Oparator file added

---
+++ Oparator.cs
@@ -0,0 +1,11 @@
+using System;
+namespace Practice
+{
+    internal class Oparator{
+
+        public static void main(string[]args){
+
+        }
+
+}
+}
--- obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+022388389f2be69d615a60cd503dbf0f3960661b")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
